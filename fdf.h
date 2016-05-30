fbe76661984fdb5d9db14aad2faf621d45920524
David WELMAN <daviwel@e5r7p1.jnb.42.fr>
2016-05-30 07:52:08 +0200

    Added minilibx and shell file

--- fdf.h
+++ fdf.h
@@ -6,17 +6,19 @@
 /*   By: ddu-toit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/27 12:30:36 by ddu-toit          #+#    #+#             */
-/*   Updated: 2016/05/27 16:19:13 by ddu-toit         ###   ########.fr       */
+/*   Updated: 2016/05/30 07:51:32 by daviwel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-# include <fcntl.h>
+# include <libc.h>
 # include <math.h>
-# include "libft/ft_printf.h"
-# include <stdio.h>
+# include "libft.h"
+# include "mlx.h"
+# define WIN_X 400
+# define WIN_Y 400
 
 typedef struct		s_point
 {
@@ -31,15 +33,24 @@ typedef struct		s_mapinfo
 	int		lines;
 }					t_mapinfo;
 
-t_point		new_point(float x, float y, float z);
-void		translate_point(float x, float y, float z, t_point *point);
-void		scale_cloud(t_point *point, float scale_f, int points);
-void		project_mod(t_point *point, int points, float mod);
-void		rotate_x(float angle, t_point *pt, int points);
-void		rotate_y(float angle, t_point *pt, int points);
-void		rotate_z(float angle, t_point *pt, int points);
-int			count_lines(char *filename);
-t_mapinfo	read_map(char* filename);
-int			map(t_mapinfo file, t_point **cloud);
+t_point				new_point(float x, float y, float z);
+
+void				translate_point(float x, float y, float z, t_point *point);
+
+void				scale_cloud(t_point *point, float scale_f, int points);
+
+void				project_mod(t_point *point, int points, float mod);
+
+void				rotate_x(float angle, t_point *pt, int points);
+
+void				rotate_y(float angle, t_point *pt, int points);
+
+void				rotate_z(float angle, t_point *pt, int points);
+
+int					count_lines(char *filename);
+
+t_mapinfo			read_map(char* filename);
+
+int					map(t_mapinfo file, t_point **cloud);
 
 #endif
