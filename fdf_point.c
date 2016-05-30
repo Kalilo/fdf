15a03508d0997e79e3cb12c0409210f782f27633
Dean DU TOIT <ddu-toit@e5r7p3.jnb.42.fr>
2016-05-30 09:38:08 +0200

    fdf_point.c updated

--- fdf_point.c
+++ fdf_point.c
@@ -6,14 +6,13 @@
 /*   By: ddu-toit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/27 13:21:30 by ddu-toit          #+#    #+#             */
-/*   Updated: 2016/05/30 08:32:59 by daviwel          ###   ########.fr       */
+/*   Updated: 2016/05/27 13:55:16 by ddu-toit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*Manipulate t_points*/
-#include "fdf.h"
 
-t_point new_point(float x, float y, float z)
+t_point new_point(int x, int y, int z)
 {
 	t_point	new;
 
@@ -21,39 +20,4 @@ t_point new_point(float x, float y, float z)
 	new.y = y;
 	new.z = z;
 	return (new);
-}
-
-/*Will offset coordinates by respective values*/
-void	translate_point(float x, float y, float z, t_point *point)
-{
-	point->x += x;
-	point->y += y;
-	point->z += z;
-}
-
-void	scale_cloud(t_point *point, float scale_f, int points)
-{
-	int i;
-
-	i = 0;
-	while (i < points)
-	{
-		point[i].x *= scale_f;
-		point[i].y *= scale_f;
-		point[i].z *= scale_f;
-		i++;
-	}
-}
-
-void	project_mod(t_point *point, int points, float mod)
-{
-	int	i;
-
-	i = 0;
-	while (i < points)
-	{
-		point[i].x *= mod;
-		point[i].y *= mod;
-		i++;
-	}
 }
