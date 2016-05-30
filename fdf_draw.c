3136d158862e00fa4b00d3fcb1a24fecebeccce2
David WELMAN <daviwel@e5r7p1.jnb.42.fr>
2016-05-30 10:10:25 +0200

    Fixed repo

--- fdf_draw.c
+++ fdf_draw.c
@@ -6,32 +6,69 @@
 /*   By: daviwel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/30 07:53:36 by daviwel           #+#    #+#             */
-/*   Updated: 2016/05/30 09:01:58 by daviwel          ###   ########.fr       */
+/*   Updated: 2016/05/30 09:52:44 by daviwel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static void	ft_draw_map(t_mapinfo mapinfo)
+static void	ft_drawline(t_point a, t_point b, t_mapinfo mapinfo)
 {
 	int	x;
 	int	y;
-	int	i;
-	int	k;
+	int	to_x;
+	int	to_y;
+
+	if (a.x > b.x)
+	{
+		x = b.x;
+		to_x = a.x;
+	}
+	else
+	{
+		x = a.x;
+		to_x = b.x;
+	}
+	if (a.z > b.z)
+	{
+		y = b.z;
+		to_y = a.z;
+	}
+	else
+	{
+		y = a.z;
+		to_y = b.z;
+	}
+	while (y < to_y && x < to_x)
+	{
+		mlx_pixel_put(mapinfo.mlx, mapinfo.win, x * 20, y * 20, 0x00FFFFFF);
+		x++;
+		y++;
+	}
+}
+
+static void	ft_draw_map(t_mapinfo mapinfo)
+{
+	int		x;
+	int		y;
+	t_point	point;
+	int		lp;
 
 	y = 0;
-	k = 0;
+	lp = 0;
 	while (y < mapinfo.lines)
 	{
 		x = 0;
 		while (x < mapinfo.points_num / mapinfo.lines)
 		{
-			i = 0x00FFFFFF;
-			if (mapinfo.points[k].y > 0)
-				i = 0x00FF0000;
-			mlx_pixel_put(mapinfo.mlx, mapinfo.win, x * 20, y * 20, i);
+			point = get_point(x, y, mapinfo);
+			printf("%f %f %f\n", point.x, point.y, point.z);
+			//mlx_pixel_put(mapinfo.mlx, mapinfo.win, x * 20, y * 20, i);
+			if (lp == 1)
+				ft_drawline(mapinfo.lastpoint, point, mapinfo);
+			mapinfo.lastpoint = point;
 			x++;
-			k++;
+			lp = 1;
 		}
 		y++;
 	}
