1dc16daad045d0d8596936701ee58a7321026dd4
Dean DU TOIT <ddu-toit@e5r7p3.jnb.42.fr>
2016-05-31 15:12:06 +0200

    rotation changes

--- fdf_point_rotation.c
+++ fdf_point_rotation.c
@@ -6,18 +6,46 @@
 /*   By: ddu-toit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/27 13:21:30 by ddu-toit          #+#    #+#             */
-/*   Updated: 2016/05/31 13:46:15 by ddu-toit         ###   ########.fr       */
+/*   Updated: 2016/05/31 15:06:44 by ddu-toit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*Manipulate t_points*/
 #include "fdf.h"
 
+t_point	fix_neg(t_mapinfo *map)
+{
+	t_point	ret;
+	int	i;
+	int k;
+
+	i = 0;
+	k = 0;
+	ret = new_point(0 , 0, 0);
+ 		while (i < map->lines)
+		{
+			k = 0;
+			while (k < map->width)
+ 			{
+				if (map->points[i][k].x < ret.x)
+					ret.x = map->points[i][k].x;
+				if (map->points[i][k].y < ret.y)
+					ret.y = map->points[i][k].y;
+				if (map->points[i][k].z < ret.z)
+					ret.y = map->points[i][k].z;
+ 				k++;
+			}
+			i++;
+		}
+	translate_point(ft_abs(ret.x), ft_abs(ret.y), ft_abs(ret.z), map);
+}
+
 void	translate_rot(float angle, t_mapinfo *map, void (f)(float, t_mapinfo *))
 {
-	int	c_x;
-	int	c_y;
-	int	c_z;
+	int		c_x;
+	int		c_y;
+	int		c_z;
+	t_point	neg_corr;
 
 	c_x = map->points[map->lines / 2][map->width / 2].x;
 	c_y = map->points[map->lines / 2][map->width / 2].y;
@@ -28,6 +56,7 @@ void	translate_rot(float angle, t_mapinfo *map, void (f)(float, t_mapinfo *))
 	printf("(%d ; %d ; %d)\n", map->points[0][0].x, map->points[0][0].y, map->points[0][0].z);
 	f(angle, map);
 	translate_point(c_x, c_y, c_z, map);
+	fix_neg(map);
 	printf("(%d ; %d ; %d)\n", map->points[0][0].x, map->points[0][0].y, map->points[0][0].z);
 }
 
@@ -37,7 +66,6 @@ void	rotate_x(float angle, t_mapinfo *map)
 	float	z1;
 	int		u;
 	int		v;
-
 
 	v = 0;
 	while (v < map->lines)
