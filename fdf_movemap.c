7d5401637ca12dced9ef28bc7873359bbdcf4a5b
David WELMAN <daviwel@e5r7p1.jnb.42.fr>
2016-05-31 11:02:42 +0200

    stuff

--- fdf_movemap.c
+++ fdf_movemap.c
@@ -6,41 +6,33 @@
 /*   By: daviwel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/31 09:11:32 by daviwel           #+#    #+#             */
-/*   Updated: 2016/05/31 10:23:58 by daviwel          ###   ########.fr       */
+/*   Updated: 2016/05/31 11:01:31 by daviwel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-t_point	ft_findcentre(t_mapinfo map)
-{
-	return (map.points[map.scale_x / 2][map.scale_y / 2]);
-}
-
 void	ft_centremap(t_mapinfo *map)
 {
-	/*int		offset_x;
-	int		offset_y;
-	t_point	centre;
-
-	centre = ft_findcentre(*map);
-	//printf("Hi");
-	//printf("%d %d", centre.x, centre.z);
-	offset_x = WIN_X / 2 - centre.x;
-	offset_y = WIN_Y / 2 - centre.z;*/
 	int	i;
 	int	k;
+	int	offset_x;
+	int	offset_y;
 
 	i = 0;
+	offset_x = map->points[map->lines / 2][map->width / 2].x;
+	offset_y = map->points[map->lines / 2][map->width / 2].z;
 	while (i < map->lines)
 	{
+		k = 0;
 		while (k < map->width)
 		{
-			map->points[i][k].x += WIN_X / 2;
-			map->points[i][k].z += WIN_Y / 2;
+			map->points[i][k].x -= offset_x;
+			map->points[i][k].x += 200;
+			map->points[i][k].z -= offset_y;
+			map->points[i][k].z += 50;
 			k++;
 		}
 		i++;
-	}
-	//ft_movemap(map, offset_x, offset_y);
+	}	
 }
