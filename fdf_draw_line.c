ea1adfc52e35da2645efed85e031dc67926cfd8c
Reyno VAN DER WESTHUIZEN <reyvan-d@e5r7p2.jnb.42.fr>
2016-06-03 20:51:22 +0200

    draw_line

--- fdf_draw_line.c
+++ fdf_draw_line.c
@@ -3,34 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   fdf_draw_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ddu-toit <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: reyvan-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/06/02 15:55:28 by ddu-toit          #+#    #+#             */
-/*   Updated: 2016/06/02 15:55:30 by ddu-toit         ###   ########.fr       */
+/*   Created: 2016/06/03 19:51:54 by reyvan-d          #+#    #+#             */
+/*   Updated: 2016/06/03 20:40:03 by reyvan-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void			draw_line(t_point p1, t_point p2, t_mapinfo *m)
+void	draw_line(t_point p1, t_point p2, t_mapinfo *m)
 {
-	float	step;
-	float	k;
-	int		col;
-	t_point	sum;
+	int dx = abs(p2.x-p1.x), sx = p1.x<p2.x ? 1 : -1;
+	int dy = abs(p2.y-p1.y), sy = p1.y<p2.y ? 1 : -1; 
+	int err = (dx>dy ? dx : -dy)/2, e2;
 
-	k = 0;
-	if (p1.col == 1 && p2.col == 1)
-		col = RED;
-	else
-		col = BROWN;
-	step = (float)pow((fmax(CON_TWO, CON_THREE)), -1);
-	while (k <= 1)
-	{
-		sum.x = p1.x + k * (p2.x - p1.x);
-		//sum.y = p1.y + k * (p2.y - p1.y);
-		sum.z = p1.z + k * (p2.z - p1.z);
-		mlx_pixel_put(m->mlx, m->win, sum.x, sum.z, col);
-		k += step;
+	for(;;){
+	mlx_pixel_put(m->mlx, m->win, p1.x, p1.y, 0xFFFFFF);
+	if (p1.x == p2.x && p1.y == p2.y) break;
+	e2 = err;
+	if (e2 >-dx) { err -= dy; p1.x += sx; }
+	if (e2 < dy) { err += dx; p1.y += sy; }
 	}
 }
